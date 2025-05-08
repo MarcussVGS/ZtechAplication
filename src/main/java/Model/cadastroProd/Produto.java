dc47a2dedb66b198e1ede520f84c943e55c22a56
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:05:35 -0300

    add classe produto

--- src/main/java/Model/cadastroProd/Produto.java
+++ src/main/java/Model/cadastroProd/Produto.java
@@ -1,5 +1,68 @@
 package Model.cadastroProd;
 
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Produto {
+	
+	@Id
+	private int idProduto;
+	@NotEmpty
+	private String nomeProduto;
+	@NotEmpty
+	private float custo;
+	@NotEmpty
+	private String descricao;
+	@NotEmpty
+	private Marca marca;
+	@NotEmpty
+	private Categoria categoria;
+	
+	public int getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(int idProduto) {
+		this.idProduto = idProduto;
+	}
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	
+	public float getCusto() {
+		return custo;
+	}
+	public void setCusto(float custo) {
+		this.custo = custo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	//chaves estrangeiras
+	public Marca getMarca() {
+		return marca;
+	}
+	public void setMarca(Marca marca) {
+		this.marca = marca;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 
 }
