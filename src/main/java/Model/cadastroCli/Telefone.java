7968d15be059bac792129bd0682babe50c1bcedf
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:12:45 -0300

    update classe telefone

--- src/main/java/Model/cadastroCli/Telefone.java
+++ src/main/java/Model/cadastroCli/Telefone.java
@@ -1,5 +1,15 @@
 package Model.cadastroCli;
 
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Telefone {
+	
+	@Id
+	private int idTelefone;
+	@NotEmpty
+	private String telefone;
 
 }
