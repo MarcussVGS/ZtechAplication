b4955eda2dd4260b5f1beb8dd00dadc66f789fc6
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:15:57 -0300

    update classe cliente

--- src/main/java/Model/cadastroCli/Cliente.java
+++ src/main/java/Model/cadastroCli/Cliente.java
@@ -1,5 +1,64 @@
 package Model.cadastroCli;
 
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Cliente {
+	
+	@Id
+	private int idCliente;
+	@NotEmpty
+	private String nomeCliente;
+	@NotEmpty
+	private String cpf;
+	@NotEmpty
+	private Email email;
+	@NotEmpty
+	private Endereco endereco;
+	@NotEmpty
+	private Telefone telefone;
+	
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	//chaves estrangeiras
+	public Email getEmail() {
+		return email;
+	}
+	public void setEmail(Email email) {
+		this.email = email;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public Telefone getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+	
+	
 
 }
