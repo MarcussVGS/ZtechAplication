50b92a0dbfa30815e2d69e45c854478d0b1f6432
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:09:49 -0300

    update classe endereco

--- src/main/java/Model/cadastroCli/Endereco.java
+++ src/main/java/Model/cadastroCli/Endereco.java
@@ -1,5 +1,62 @@
 package Model.cadastroCli;
 
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Endereco {
+	
+	@Id
+	private int idEndereco;
+	@NotEmpty
+	private String rua;
+	@NotEmpty
+	private String cep;
+	@NotEmpty
+	private String bairro;
+	@NotEmpty
+	private String cidade;
+	@NotEmpty
+	private int numeroCasa;
+	
+	public int getIdEndereco() {
+		return idEndereco;
+	}
+	public void setIdEndereco(int idEndereco) {
+		this.idEndereco = idEndereco;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public int getNumeroCasa() {
+		return numeroCasa;
+	}
+	public void setNumeroCasa(int numeroCasa) {
+		this.numeroCasa = numeroCasa;
+	}
+	
+	
 
 }
