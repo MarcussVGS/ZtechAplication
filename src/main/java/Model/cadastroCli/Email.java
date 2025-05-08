c882e9583e9bcc549e6ce210cc20c99ddba336f0
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:11:26 -0300

    update classe email

--- src/main/java/Model/cadastroCli/Email.java
+++ src/main/java/Model/cadastroCli/Email.java
@@ -1,5 +1,30 @@
 package Model.cadastroCli;
 
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Email {
+	
+	@Id
+	private int idEmail;
+	@NotEmpty
+	private String email;
+	
+	public int getIdEmail() {
+		return idEmail;
+	}
+	public void setIdEmail(int idEmail) {
+		this.idEmail = idEmail;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 
 }
