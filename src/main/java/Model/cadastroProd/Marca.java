9f57b5e6c18d4842901ae00961b7543803983efb
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:00:31 -0300

    classe Marca

--- src/main/java/Model/cadastroProd/Marca.java
+++ src/main/java/Model/cadastroProd/Marca.java
@@ -1,5 +1,31 @@
 package Model.cadastroProd;
 
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Marca {
+	
+	@Id
+	private int idMarca;
+	@NotEmpty
+	private String nomeMarca;
+	
+	public int getIdMarca() {
+		return idMarca;
+	}
+	public void setIdMarca(int idMarca) {
+		this.idMarca = idMarca;
+	}
+	
+	public String getNomeMarca() {
+		return nomeMarca;
+	}
+	public void setNomeMarca(String nomeMarca) {
+		this.nomeMarca = nomeMarca;
+	}
+	
+	
 
 }
