5f694eb3564edb1842abcf6c9c4e5ad0d2cc4fd2
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:22:10 -0300

    update classe estoque

--- src/main/java/Model/Estoque.java
+++ src/main/java/Model/Estoque.java
@@ -1,5 +1,39 @@
 package Model;
 
+import Model.cadastroProd.*;
+
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Estoque {
+	
+	@Id
+	private int idEstoque ;
+	@NotEmpty
+	private int quantidade;
+	@NotEmpty
+	private Produto produto;
+	
+	public int getIdEstoque() {
+		return idEstoque;
+	}
+	public void setIdEstoque(int idEstoque) {
+		this.idEstoque = idEstoque;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
 
 }
