36a304898cf9a8ee30947a8fc77dc76a1007626a
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-08 14:19:52 -0300

    update classe servico

--- src/main/java/Model/Servico.java
+++ src/main/java/Model/Servico.java
@@ -1,5 +1,38 @@
 package Model;
 
+import java.io.Serializable;
+import java.util.List;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+
 public class Servico {
+	
+	@Id
+	private int idServico;
+	@NotEmpty
+	private String descrisaoServico;
+	@NotEmpty
+	private float valor;
+	
+	public int getIdServico() {
+		return idServico;
+	}
+	public void setIdServico(int idServico) {
+		this.idServico = idServico;
+	}
+	public String getDescrisaoServico() {
+		return descrisaoServico;
+	}
+	public void setDescrisaoServico(String descrisaoServico) {
+		this.descrisaoServico = descrisaoServico;
+	}
+	public float getValor() {
+		return valor;
+	}
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
+	
 
 }
