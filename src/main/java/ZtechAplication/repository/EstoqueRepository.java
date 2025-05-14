93bd9aa57b57d548ad7999d961937ffc76e15f93
MarcusSampaio <marcuss.sampaioo@gmail.com>
2025-05-14 08:04:10 -0300

    create - estoque controller e repository

---
+++ src/main/java/ZtechAplication/repository/EstoqueRepository.java
@@ -0,0 +1,9 @@
+package ZtechAplication.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ZtechAplication.model.Estoque;
+
+public interface EstoqueRepository extends CrudRepository<Estoque, Long> {
+
+}
